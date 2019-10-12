e448e99c82e41346e0ac2e711bddf6fca1d97ce6
Lucas Wang <lucaswzx@icloud.com>
2019-10-12 15:59:36 -0700

    added class, trying to do array

--- Feel Better/Feel Better/Memories Class.swift
+++ Feel Better/Feel Better/Memories Class.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2019 Feel Better. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // The class for memeories stored as objects in local array and FireBase
-
+class Memory {
+	var title: String
+	var content: String
+	var sentiment: Int
+	var saved_date: Date
+	var image: UIImage?
+	
+	init? (title: String, content: String, sentiment: String, saved_date: Date, image: UIImage?)
+	
+	self.title = title
+	self.content = content
+	self.sentiment = sentiment
+	self.saved_date = saved_date
+	self.image = image
+	
+	guard !title.isEmpty else {
+		return nil
+	}
+}

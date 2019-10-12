047561cd2b60b4bf0adb4b44cc1828fcf54a3790
ApolloZhu <public-apollonian@outlook.com>
2019-10-12 13:54:22 -0700

    Create project

---
+++ Feel Better/Feel Better/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  Feel Better
+//
+//  Created by Apollo Zhu on 10/12/19.
+//  Copyright © 2019 Feel Better. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Feel Better/Feel Better/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  Feel Better
+//
+//  Created by Apollo Zhu on 10/12/19.
+//  Copyright © 2019 Feel Better. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

7da39c138922967024388c99a135c71db8fc8531
Lucas Wang <lucaswzx@icloud.com>
2019-10-12 15:34:41 -0700

    Added some good stuff

---
+++ Feel Better/Feel Better/Memories Class.swift
@@ -0,0 +1,12 @@
+//
+//  Memories Class.swift
+//  Feel Better
+//
+//  Created by Lucas Wang on 2019-10-12.
+//  Copyright © 2019 Feel Better. All rights reserved.
+//
+
+import Foundation
+
+// The class for memeories stored as objects in local array and FireBase
+

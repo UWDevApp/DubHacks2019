578c9f10dd4cbfee6aa01be7f2c2e42110775a8e
Lucas Wang <lucaswzx@icloud.com>
2019-10-12 14:31:55 -0700

    Testing testing first push

--- Feel Better/Feel Better/MemoriesTableViewViewController.swift
+++ Feel Better/Feel Better/MemoriesTableViewViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class MemoriesTableViewViewController: UITableViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

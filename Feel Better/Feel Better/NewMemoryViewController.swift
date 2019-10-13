dcd9d5377504053d676d2104510ca80217fe907f
Lucas Wang <lucaswzx@icloud.com>
2019-10-12 19:17:45 -0700

    pre Swift Package Manager Import FreshOS / KeyboardLayoutGuide commit

---
+++ Feel Better/Feel Better/NewMemoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewMemoryViewController.swift
+//  Feel Better
+//
+//  Created by Lucas Wang on 2019-10-12.
+//  Copyright © 2019 Feel Better. All rights reserved.
+//
+
+import UIKit
+
+class NewMemoryViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+	
+	
+}

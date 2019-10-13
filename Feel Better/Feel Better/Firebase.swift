a7b9463109aa191b505003c4bd5287dcab16fbe0
ApolloZhu <public-apollonian@outlook.com>
2019-10-13 03:37:29 -0700

    Integrate Memory and firebase

--- Feel Better/Feel Better/Firebase.swift
+++ Feel Better/Feel Better/Firebase.swift
@@ -18,7 +18,6 @@ public class Firebase {
     
     private let db = Firestore.firestore()
     private lazy var diariesRef = db.collection("diaries")
-    private lazy var docRef = db.collection("diaries").document("today")
     private let storage = Storage.storage()
     private lazy var storageRef = storage.reference()
     private let ref: DatabaseReference = Database.database().reference()
@@ -27,92 +26,70 @@ public class Firebase {
     // ref.child("name").childByAutoId().setValue("visual")
     // ref.child("name").childByAutoId().setValue("phanith")
     
-    func setDocument() {
+    func saveNewMemory(_ memory: LocalMemory, tags: [String]) {
         // Add a new document in collection "diaries"
-        db.collection("diaries").document("today").setData([
-            "title": "Journal",
-            "content": "content",
-            "sentiments":"happy",
-            "saveDate": "10/12/19"
+        let newMemoryRef = diariesRef.document()
+        let documentID = newMemoryRef.documentID
+        
+        if let data = memory.image?.pngData() {
+            uploadImage(data, for: documentID)
+        }
+        
+        // var image: UIImage?
+        newMemoryRef.setData([
+            "title": memory.title,
+            "content": memory.content,
+            "sentiments": memory.sentiment,
+            "saveDate": Timestamp(date: memory.saveDate),
+            "tags": tags
         ]) { err in
             if let err = err {
                 print("Error writing document: \(err)")
             } else {
-                print("Document successfully written!")
+                print("Document \(documentID) successfully written!")
             }
         }
     }
     
-    func readData() {
-        // read the data
-        diariesRef.document("diaries").setData([
-            "title": "Jorunal",
-            "content": "content",
-            "sentiments":"happy",
-            "saveDate": "10/12/19"
-        ])
-    }
-    
-    func getDoc() {
-        //get the document from designated collection
-        docRef.getDocument {(document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                print("Document data: \(dataDescription)")
-            } else {
+    func getMemory(withID documentID: String, then process: @escaping (Result<CloudMemory, Error>) -> Void) {
+        // get the document from designated collection
+        diariesRef.document(documentID).getDocument { [unowned self] (document, error) in
+            guard let data = document?.data() else {
                 print("Document does not exist")
+                return
+            }
+            guard let title = data["title"] as? String
+                , let content = data["content"] as? String
+                , let sentiment = data["sentiment"] as? Int
+                , let saveTimestamp = data["saveDate"] as? Timestamp
+                , let tags = data["tags"] as? [String]
+                else {
+                    return print("Wrong memory format: \(data)")
+            }
+            self.getImage(for: documentID) { (result) in
+                process(result.map {
+                    CloudMemory(documentID: documentID, title: title, content: content,
+                    sentiment: sentiment, tags: tags,
+                    saveDate: saveTimestamp.dateValue(), imageURL: $0)
+                })
             }
         }
     }
     
-    func uploadFilesFromMemory(data: Data, ErrorReporter : (String) -> Void){
-        
+    func uploadImage(_ data: Data, for documentID: String){
         // Create a reference to the file you want to upload
-        let picsRef = storageRef.child("images/user.jpg")
-        
+        let picsRef = storageRef.child(documentID + ".png")
         // Upload the file to the path "images/rivers.jpg"
-        _ = picsRef.putData(data, metadata: nil) { (metadata, error) in
-            guard let metadata = metadata else {
-                // Uh-oh, an error occurred!
-                return
-            }
-            // Metadata contains file metadata such as size, content-type.
-            _ = metadata.size
-            
-            // You can also access to download URL after upload.
-            picsRef.downloadURL { (url, error) in
-                guard let downloadURL = url else {
-                    // Uh-oh, an error occurred!
-                    return
-                }
-            }
-        }
+        picsRef.putData(data, metadata: nil)
     }
     
-    
-    func uploadFilesFromLocal(ErrorReporter : (String) -> Void) -> Void {
-        // File located on disk
-        let localFile = URL(string: "path/to/file")!
-        
-        // Create a reference to the file you want to upload
-        let picsRef = storageRef.child("images/user.jpg")
-        
-        // Upload the file to the path "images/rivers.jpg"
-        _ = picsRef.putFile(from: localFile, metadata: nil) { metadata, error in
-            guard let metadata = metadata else {
-                // Uh-oh, an error occurred!
-                return
-            }
-            // Metadata contains file metadata such as size, content-type.
-            _ = metadata.size
-            // You can also access to download URL after upload.
-            picsRef.downloadURL { (url, error) in
-                guard let downloadURL = url else {
-                    // Uh-oh, an error occurred!
-                    
-                    return
-                    
-                }
+    func getImage(for documentID: String, then process: @escaping (Result<URL, Error>) -> Void) {
+        let picsRef = storageRef.child(documentID + ".png")
+        picsRef.downloadURL { (url, error) in
+            if let url = url {
+                process(.success(url))
+            } else {
+                process(.failure(error!))
             }
         }
     }
--- Feel Better/Feel Better/Memories Class.swift
+++ Feel Better/Feel Better/Memories Class.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 // The class for memeories stored as objects in local array and FireBase
-struct Memory {
-	var title: String
-	var content: String
-	var sentiment: Int
-	var saveDate: Date
-	var image: UIImage?
-	
-	var sentimentEmoji: String {
-		switch sentiment {
-			case ..<20:
-				return "😭"
-			case 20..<40:
-				return "☹️"
-			case 40..<60:
-				return "😐"
-			case 60..<80:
-				return "🙂"
-			case 80...:
-				return "😄"
-			default:
-				return "GG"
-		}
-	}
+protocol MemoryType {
+    var title: String { get }
+    var content: String { get }
+    var sentiment: Int { get }
+    var saveDate: Date { get }
+}
+
+extension MemoryType {
+    var sentimentEmoji: String {
+        switch sentiment {
+        case ..<20:
+            return "😭"
+        case 20..<40:
+            return "☹️"
+        case 40..<60:
+            return "😐"
+        case 60..<80:
+            return "🙂"
+        case 80...:
+            return "😄"
+        default:
+            fatalError("This should never happen")
+        }
+    }
+}
+
+@available(*, deprecated, renamed: "LocalMemory")
+typealias Memory = LocalMemory
+
+struct LocalMemory: MemoryType {
+    var title: String
+    var content: String
+    var sentiment: Int
+    var saveDate: Date
+    var image: UIImage?
+}
+
+struct CloudMemory: MemoryType {
+    let documentID: String
+    
+    let title: String
+    let content: String
+    let sentiment: Int
+    let tags: [String]
+    let saveDate: Date
+    let imageURL: URL?
 }
